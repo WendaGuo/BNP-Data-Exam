51e0dd93f0a4df843831cb0f1a0830ad2ffd11e4
Wenda Guo <wg950914@gmail.com>
2020-04-28 22:33:19 -0400

    second commit

--- summarize-enron.py
+++ summarize-enron.py
@@ -11,33 +11,25 @@ def parse_pipe_separated_col(recipients: str) -> List[str]:
     A function that transforms pipe separated recipients to recipient list
     Passed as a function parameter in `pd.Series.apply`
     """
-    recipients = recipients.strip()
-    recipients_list = recipients.split('|')
-    return recipients_list
+    return recipients.strip().split('|')
 
 
-def count_sender(data: pd.DataFrame) -> pd.DataFrame:
+def count_sent_received_on_person(data: pd.DataFrame, exploded_data: pd.DataFrame) -> pd.DataFrame:
     """
-    count number of sending from each individual
+    This function output a csv file that is required by question (1)
+    :param data: raw_data
+    :param exploded_data: recipients_exploded_data
+    :return:
     """
-    group_sender = data.groupby('sender')['message identifier'].count()
-    group_sender = pd.DataFrame(group_sender)
-    group_sender = group_sender.rename_axis('person')
-    group_sender = group_sender.rename(columns={'message identifier': 'sent'})
+    group_sender = data.groupby('sender')['message identifier'].count().rename('sent')
+    group_receiver = exploded_data.groupby('recipients')['message identifier'].count().rename('received')
+    result = pd.concat([group_sender, group_receiver], axis=1) \
+        .fillna(0).astype(np.int64).sort_values('sent')
 
-    return group_sender
-
-
-def count_receiver(data: pd.DataFrame) -> pd.DataFrame:
-    """
-    count number of email received of each individual
-    """
-    group_receiver = data.groupby('recipients')['message identifier'].count()
-    group_receiver = pd.DataFrame(group_receiver)
-    group_receiver = group_receiver.rename_axis('person')
-    group_receiver = group_receiver.rename(columns={'message identifier': 'received'})
-
-    return group_receiver
+    csv_path = os.getcwd() + '/person-sent-received.csv'
+    result.to_csv(csv_path, index_label='person')
+    print("csv file successfully saved, please check at {}".format(csv_path))
+    return result
 
 
 def plot_email_num_overtime(data: pd.Series, derived_data: pd.DataFrame) -> None:
@@ -56,23 +48,23 @@ def plot_email_num_overtime(data: pd.Series, derived_data: pd.DataFrame) -> None
     ax1 = plt.subplot(212)
     ax1.bar(sent.index, sent / weeks, align='center')
     ax1.set_xticklabels(sent.index, rotation=90)
-    ax1.set_title("Number of individual's weekly average sent")
-    ax1.set_xlabel('individuals')
-    ax1.set_ylabel('number of weekly average sent')
+    ax1.set_title("Number of individual's weekly average sent", fontsize=20)
+    ax1.set_xlabel('individuals', fontsize=15)
+    ax1.set_ylabel('number of weekly average sent', fontsize=15)
 
     ax2 = plt.subplot(221)
     ax2.plot(data.index, data)
     ax2.set_xticks(data.index[::25])
     ax2.set_xticklabels(data.index[::25], rotation=45)
-    ax2.set_xlabel('weeks (format of %Y-%U)')
-    ax2.set_ylabel('number of weekly sent')
-    ax2.set_title('changing of accumulative weekly sent over time (the most prolific 100 individuals)')
+    ax2.set_xlabel('weeks (format of %Y-%U)', fontsize=15)
+    ax2.set_ylabel('number of weekly sent', fontsize=15)
+    ax2.set_title('changing of accumulative weekly sent over time (the most prolific 100 individuals)', fontsize=20)
 
     ax3 = plt.subplot(222)
     ax3.hist(sent / weeks, density=False, bins=30)
-    ax3.set_xlabel('number of average weekly sent by one individual')
-    ax3.set_ylabel('frequency / count')
-    ax3.set_title('Distribution of average weekly sent (among the most prolific 100 individuals)')
+    ax3.set_xlabel('number of average weekly sent by one individual', fontsize=15)
+    ax3.set_ylabel('frequency / count', fontsize=15)
+    ax3.set_title('Distribution of average weekly sent (among the most prolific 100 individuals)', fontsize=20)
 
     save_path = os.getcwd() + "/Visualization (2).png"
     plt.savefig(save_path)
@@ -94,15 +86,15 @@ def plot_unique_contacts(relative: pd.DataFrame, weekly_sent: pd.DataFrame,
     axes[0].legend(loc='upper right')
     axes[0].set_xticks(unique_contacts.index[::10])
     axes[0].set_xticklabels(unique_contacts.index[::10], rotation=45)
-    axes[0].set_title('changing of sending and unique contacts over time (among the most prolific 100 individuals)')
-    axes[0].set_xlabel('weeks (format of %Y-%U)')
-    axes[0].set_ylabel('number of emails sent/received')
+    axes[0].set_title('changing of sending and unique contacts over time (among the most prolific 100 individuals)', fontsize=15)
+    axes[0].set_xlabel('weeks (format of %Y-%U)', fontsize=15)
+    axes[0].set_ylabel('number of emails sent/received', fontsize=15)
 
     axes[1].hist(relative['relative number'], density=False, bins=30)
     axes[1].set_title(
-        'Distribution of relative number (ratio of unique contacts over number of sending) of the same person')
-    axes[1].set_xlabel('relative number')
-    axes[1].set_ylabel('frequency / count')
+        'Distribution of relative number (ratio of unique contacts over number of sending) of the same person', fontsize=15)
+    axes[1].set_xlabel('relative number', fontsize=15)
+    axes[1].set_ylabel('frequency / count', fontsize=15)
 
     plt.subplots_adjust(wspace=0.1)
     save_path = os.getcwd() + '/Visualization (3).png'
@@ -111,34 +103,28 @@ def plot_unique_contacts(relative: pd.DataFrame, weekly_sent: pd.DataFrame,
 
 
 if __name__ == "__main__":
+    plt.style.use('seaborn')
     file_name = sys.argv[1]
     raw_data = pd.read_csv(file_name,
                            names=['time', 'message identifier', 'sender',
                                   'recipients', 'topic', 'mode'],
                            index_col=0)
     raw_data['recipients'] = raw_data['recipients'].astype(str)
-
-    group_sender = count_sender(raw_data)
-
+    # parse pipe-separated string to list and expand them as cloumn value
     raw_data['recipients'] = raw_data['recipients'].apply(parse_pipe_separated_col)
     recipients_exploded_data = raw_data.explode('recipients')
-    group_receiver = count_receiver(recipients_exploded_data)
 
-    person_sent_received = group_sender.merge(group_receiver, how='outer', on='person')
-    person_sent_received = person_sent_received.fillna(0).astype(np.int64).sort_values('sent')
-
-    csv_path = os.getcwd() + '/person-sent-received.csv'
-    person_sent_received.to_csv(csv_path)
-    print("csv file successfully saved, please check at {}".format(csv_path))
-
+    person_sent_received = count_sent_received_on_person(raw_data,recipients_exploded_data)
+    # prepare data for visualization (2)
     prolific = raw_data[raw_data.sender.isin(person_sent_received.index[-100:])]
     prolific.index = pd.to_datetime(prolific.index, unit='ms')
     prolific = prolific.reset_index()
     prolific['time'] = prolific['time'].dt.strftime('%Y-%U')
     weekly_sent = prolific.groupby('time')['message identifier'].count()
     plot_email_num_overtime(weekly_sent, person_sent_received)
-
-    contacts = recipients_exploded_data[recipients_exploded_data.recipients.isin(person_sent_received.index[-100:])].reset_index()
+    # prepare data for visualization (3)
+    contacts = recipients_exploded_data[
+        recipients_exploded_data.recipients.isin(person_sent_received.index[-100:])].reset_index()
     contacts['time'] = pd.to_datetime(contacts['time'], unit='ms').dt.strftime('%Y-%U')
     unique_contacts = pd.DataFrame(contacts.groupby('time')['sender'].nunique())
     unique_contacts['sent'] = weekly_sent[unique_contacts.index]
@@ -147,3 +133,6 @@ if __name__ == "__main__":
     relative['sent'] = person_sent_received.loc[relative.index, 'sent']
     relative['relative number'] = relative['sender'] / relative['sent']
     plot_unique_contacts(relative, weekly_sent, unique_contacts)
+
+
+
